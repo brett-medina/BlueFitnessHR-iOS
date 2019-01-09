924d7a222b725ac32deb566af38458d2daffcfcc
Kushangupta <kushangupta@gmail.com>
2019-01-08 22:09:08 -0600

    Fixed scaling of buttons and fields, and sub classing the signup view

--- BlueFitnessHR/LoginController.swift
+++ BlueFitnessHR/LoginController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import UIGradient
 
 class LoginController: UIViewController {
 
--- BlueFitnessHR/SignupViewController.swift
+++ BlueFitnessHR/SignupViewController.swift
@@ -14,7 +14,6 @@ class SignupViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.fromGradientWithDirection(.topToBottom, frame: self.view.frame, colors: [UIColor.blue, UIColor.green])
-
         // Do any additional setup after loading the view.
     }
 
